d7e0dd0d91b49038dc8f872216dcd3945dcd6559
Acer <Acer@WIN81>
2018-08-02 09:34:21 +0700

    3

---
+++ src/j08/Contoh3.java
@@ -0,0 +1,19 @@
+package j08;
+
+public class Contoh3 {
+    public static void main(String[] args) {
+        int x;
+        
+        for(x = 1; x <= 20; x+=2)
+        {
+            System.out.println(x + " ");
+        }
+        System.out.println();
+        
+        
+        for(x = 2; x <= 20; x+=2)
+        {
+            System.out.println(x + " ");
+        }
+    }
+}
