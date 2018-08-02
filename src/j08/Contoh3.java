7254d30c22a61d21ce888a548c14086c46dce7de
Acer <Acer@WIN81>
2018-08-02 09:43:35 +0700

    contoh3

--- src/j08/Contoh3.java
+++ src/j08/Contoh3.java
@@ -3,7 +3,7 @@ package j08;
 public class Contoh3 {
     public static void main(String[] args) {
         int x;
-        
+        //cara 1
         for(x = 1; x <= 20; x+=2)
         {
             System.out.println(x + " ");
@@ -14,6 +14,19 @@ public class Contoh3 {
         for(x = 2; x <= 20; x+=2)
         {
             System.out.println(x + " ");
+        }
+        //cara 2
+        for(x = 1; x <=20; x++)
+        {
+            if(x % 2 == 1)
+            {
+                System.out.println(x +" ");
+            }
+        }
+        
+        for(x = 1; x <=20; x++)
+        {
+            
         }
     }
 }
